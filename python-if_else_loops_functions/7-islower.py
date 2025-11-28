44e69167e40644ea9fa61bcfa1a86cd751cf8235
skicodes <skalrashed02@hotmail.com>
2025-11-28 19:53:12 +0000

    8

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    """
+    Checks if the character c is lowercase.
+
+    Returns True if c is lowercase, False otherwise.
+    """
+    return ord('a') <= ord(c) <= ord('z')
