51c3c450191b7a23ecab966d15d1a6858fa27ecd
skicodes <skalrashed02@hotmail.com>
2025-12-03 23:07:06 +0000

    lkkl

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    """Print up to x elements of a list safely and return the number printed."""
-    count = 0
+    printed = 0
     for i in range(x):
         try:
             print(my_list[i], end="")
-            count += 1
-        except IndexError:
+            printed += 1
+        except Exception:
             break
     print()
-    return count
+    return printed
 
