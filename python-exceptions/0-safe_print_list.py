52fde6b3230b14595bbeb6449911933156abe43a
skicodes <skalrashed02@hotmail.com>
2025-12-03 20:14:50 +0000

    0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Print x elements of a list safely.
+    
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements to attempt to print.
+    
+    Returns:
+        int: The real number of elements printed.
+    """
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
